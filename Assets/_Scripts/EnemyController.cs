b1700b6f99fe911615ddb41a1cb9542bebe3b6a2
DiversenSato <diversensato@gmail.com>
2019-01-30 18:54:19 +0100

    Sputnik chase complete

--- Assets/_Scripts/EnemyController.cs
+++ Assets/_Scripts/EnemyController.cs
@@ -4,18 +4,44 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    public LayerMask playerLayer;
     public Transform pointA;
     public Transform pointB;
 
+    public float chaseSpeed;
     public float enemySpeed;
+    public bool isChasing = false;
+    public GameObject Player;
 
 	void Start ()
     {
-		
+        Player = GameObject.FindGameObjectWithTag("Player");
 	}
 	
 	void Update ()
     {
-        this.transform.position = Vector3.Lerp(pointA.position, pointB.position, Mathf.PingPong(Time.time * enemySpeed, 1));
-	}
+        RaycastHit2D hit = Physics2D.Raycast(this.transform.position, Vector2.left, 2, playerLayer);
+
+        if(hit.collider != null)
+        {
+            Debug.Log(hit.collider.transform.name);
+
+            if (hit.collider.gameObject.CompareTag("Player"))
+            {
+                isChasing = true;   
+            }
+
+            Debug.Log(isChasing);
+        }
+
+        if (isChasing == true)
+        {
+            this.transform.position = Vector2.MoveTowards(this.transform.position, Player.transform.position, Time.deltaTime * chaseSpeed);
+        }
+
+        if (isChasing == false)
+        {
+            this.transform.position = Vector3.Lerp(pointA.position, pointB.position, Mathf.PingPong(Time.time * enemySpeed, 1));
+        }
+    }
 }
