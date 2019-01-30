98aad3607e9362d2a577873545ad0636e9f2282d
DiversenSato <diversensato@gmail.com>
2019-01-30 16:28:53 +0100

    Import project

---
+++ Assets/_Scripts/EnemyController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float speed;
+    public Transform[] Points;
+    public Transform currenttarget;
+    public int currentindex;
+    
+	void Start ()
+    {
+        currentindex = 0;
+        currenttarget = Points[currentindex];
+    }
+	
+	void Update ()
+    {
+        if (this.transform.position == currenttarget.position)
+        {
+            currentindex++;
+
+            if (currentindex >= Points.Length)
+            {
+                currentindex = 0;
+            }
+
+            currenttarget = Points[currentindex];
+
+
+        }
+
+        this.transform.position = Vector3.MoveTowards(this.transform.position, currenttarget.position, speed * Time.deltaTime);
+	}
+}
---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Vector3 MyScale;
+    private Rigidbody2D rb;
+    public LayerMask layer;
+
+	void Start ()
+    {
+        MyScale = this.transform.localScale;
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.Translate(3 * Time.deltaTime, 0, 0);
+            MyScale.x = 2;
+            this.transform.localScale = MyScale;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.Translate(-3 * Time.deltaTime, 0, 0);
+            MyScale.x = -2;
+            this.transform.localScale = MyScale;
+        }
+
+        if (Input.GetKey(KeyCode.Space) && isgrounded())
+        {
+            rb.AddForce(Vector2.up * 100);
+        }
+    }
+
+    bool isgrounded()
+    {
+        if(Physics2D.Raycast(this.transform.position, Vector2.down, 1f, layer))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
