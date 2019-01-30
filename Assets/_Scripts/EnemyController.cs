f11d287dc5022decac2e880896b62a649c30fa47
DiversenSato <diversensato@gmail.com>
2019-01-30 17:16:59 +0100

    Added Sputnik and floor

--- Assets/_Scripts/EnemyController.cs
+++ Assets/_Scripts/EnemyController.cs
@@ -2,35 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyController : MonoBehaviour
+public class EnemyController1 : MonoBehaviour
 {
-    public float speed;
-    public Transform[] Points;
-    public Transform currenttarget;
-    public int currentindex;
-    
+    public Transform pointA;
+    public Transform pointB;
+
 	void Start ()
     {
-        currentindex = 0;
-        currenttarget = Points[currentindex];
-    }
+		
+	}
 	
 	void Update ()
     {
-        if (this.transform.position == currenttarget.position)
-        {
-            currentindex++;
-
-            if (currentindex >= Points.Length)
-            {
-                currentindex = 0;
-            }
-
-            currenttarget = Points[currentindex];
-
-
-        }
-
-        this.transform.position = Vector3.MoveTowards(this.transform.position, currenttarget.position, speed * Time.deltaTime);
+		
 	}
 }
